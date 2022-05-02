83500e2d485227b851c15714c4de695bcf881570
Miguel Angel Sanchez Zapata <miguel.sanchez@in2.es>
2022-05-02 12:23:04 +0200

    Added reaction for level buttons

--- Interface/Consumers/Levels.gd
+++ Interface/Consumers/Levels.gd
@@ -1,20 +1,26 @@
 extends Node2D
 
 var _current_scene = null
-const _BUTTON_SIZE = 50
+const _BUTTON_SIZE = 150
 const _BUTTON_GAP = 10
+
+var icon2 = preload("res://Interface/resources/UI/lvl1.png")
+
 
 func _init():
 	for i in [1,2,3]:
 		createbutton(i)
 
 func createbutton(level: int):
-	var b = Button.new()
+	var b = TextureButton.new()
+	b.texture_normal = load("res://Interface/resources/UI/lvl"+str(level)+".png")
+	b.texture_pressed = load("res://Interface/resources/UI/lvl"+str(level)+"p.png")
 	b.set_name(str(level))
-	b.text = str(level)
+	#b.text = str(level)
 	b.set_position(Vector2((_BUTTON_GAP + _BUTTON_SIZE) * level , 100))
 	b.rect_size = Vector2(_BUTTON_SIZE,_BUTTON_SIZE)
 	b.connect("pressed", self, "_button_pressed", [b])
+
 	add_child(b)
 
 func _input(event):
