092728f6857c3885d66fcb2df1dff2e3c90599af
Miguel Angel Sanchez Zapata <miguel.sanchez@in2.es>
2022-05-16 23:04:58 +0200

    limited square movement in drag and drop

--- Interface/Animations/FalseMovementAnimation.gd
+++ Interface/Animations/FalseMovementAnimation.gd
@@ -8,9 +8,10 @@ func _init(square1, square2):
 	_square2 = square2
 
 func Execute():
-	var positionTmp = _square1.position
-	_square1.position = _square2.position
-	_square2.position = positionTmp
+	if _square1.square.existsInRelation(_square2.square):
+		var positionTmp = _square1.position
+		_square1.position = _square2.position
+		_square2.position = positionTmp
 
 func isProcessing():
 	return _square1.isAnimationOnProgress() || _square2.isAnimationOnProgress()
